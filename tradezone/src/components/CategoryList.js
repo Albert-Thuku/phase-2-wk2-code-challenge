f97da6db2294061c67c12a3187b9d6f31a3d4e73
Albert Thuku <athuku02@gmail.com>
2023-05-01 22:45:31 +0300

    Edited the All category button

--- tradezone/src/components/CategoryList.js
+++ tradezone/src/components/CategoryList.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryList(){
+function CategoryList({products, onCategorySelect}){
 
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [categoryNames, setCategoryName] = useState([])
+
+    products.forEach((product)=>{
+        if(!categoryNames.includes(product.category)){
+            setCategoryName([...categoryNames, product.category])
+        }
+    })
+
+    function categorySelect(category){
+        if(category){
+            setSelectedCategory(category);
+            onCategorySelect(category);
+        } else {
+            setSelectedCategory(null);
+            onCategorySelect(null);
+        }
+    }
+
+    return(
+        <section id="categoryList">
+            <button onClick={() => categorySelect(null)}>All</button>
+            {categoryNames.map((category)=>{
+                return (
+                    <button 
+                        onClick={() => categorySelect(category)} 
+                        key={category}
+                        className={selectedCategory === category ? 'active' : ''}
+                    >
+                        {category}
+                    </button>
+                );
+            })}
+        </section>
+    );
 }
 
 export default CategoryList;
